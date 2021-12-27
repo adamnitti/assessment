1c80782eed8d9ec0b368e719034e076528060ffd
Adam Nitti <adamnitti@gmail.com>
2021-12-26 18:32:44 -0600

    Step 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [records, setRecords] = useState(null);
+    const average = (grades, length) => {
+        console.log(grades);
+        var sum = 0;
+        grades.map((grade) => {
+            sum = sum + Number(grade);
+            console.log(sum);
+        });
+        var average = sum / length;
+        return average;
+    };
+
+    // + adding the use
+    useEffect(() => {
+        getData();
+
+        // we will use async/await to fetch this data
+        async function getData() {
+            const response = await fetch(
+                'https://api.hatchways.io/assessment/students'
+            );
+            const data = await response.json();
+
+            // store the data into our records variable
+            setRecords(data);
+            console.log(data);
+        }
+    }, []); // <- you may need to put the setRecords function in this array
+
+    return (
+        <div>
+            {/* display records from the API */}
+            {records && (
+                <div>
+                    {/* loop over the records */}
+                    {records.students.map((student, id) => (
+                        <div key={id} className='image-txt-container'>
+                            <img
+                                className='image-round'
+                                src={student.pic}
+                                alt='icon'
+                            />
+                            <div>
+                                <h2 className='name'>
+                                    {student.firstName} {student.lastName}
+                                </h2>
+                                <div className='details'>
+                                    <p>Email: {student.email}</p>
+                                    <p>Company: {student.company}</p>
+                                    <p>Skill: {student.skill}</p>
+                                    <p>
+                                        Average:{' '}
+                                        {average(
+                                            student.grades,
+                                            student.grades.length
+                                        )}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
